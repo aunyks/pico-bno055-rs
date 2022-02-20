1ed33f2128f37b8404e358cc775c4d9b7355ebf3
Nash <me@aunyks.com>
2022-02-19 20:36:25 -0500

    https://www.youtube.com/watch?v=g_wlZ9IhbTs

--- scripts/debug-quaternion.py
+++ scripts/debug-quaternion.py
@@ -13,27 +13,31 @@ def log(msg):
   sys.stdout.write('\r{}'.format(msg))
   sys.stdout.flush()
 
-try:
-  # start = time.time()
-  # Serial takes these two parameters: serial device and baudrate
-  # Idk why this takes 40+ seconds to run / open
-  serial_conn = serial.Serial(PICO_SERIAL_PORT, 19200)
-  # print(time.time() - start)
-  # exit()
-  while True:
-    quaternion_data = serial_conn.read(size=16)
-    quaternion_data_type = type(quaternion_data)
-    quaternion_data_length = len(quaternion_data)
-    w = struct.unpack('f', quaternion_data[0:4])
-    x = struct.unpack('f', quaternion_data[4:8])
-    y = struct.unpack('f', quaternion_data[8:12])
-    z = struct.unpack('f', quaternion_data[12:16])
-    log('W: {} X: {} Y: {} Z: {}'.format(
-        w[0],
-        x[0],
-        y[0],
-        z[0]
+def main():
+  try:
+    # start = time.time()
+    # Serial takes these two parameters: serial device and baudrate
+    # Idk why this takes 40+ seconds to run / open
+    serial_conn = serial.Serial(PICO_SERIAL_PORT, 19200)
+    # print(time.time() - start)
+    # exit()
+    while True:
+      quaternion_data = serial_conn.read(size=16)
+      quaternion_data_type = type(quaternion_data)
+      quaternion_data_length = len(quaternion_data)
+      w = struct.unpack('f', quaternion_data[0:4])
+      x = struct.unpack('f', quaternion_data[4:8])
+      y = struct.unpack('f', quaternion_data[8:12])
+      z = struct.unpack('f', quaternion_data[12:16])
+      log('W: {} X: {} Y: {} Z: {}'.format(
+          w[0],
+          x[0],
+          y[0],
+          z[0]
+        )
       )
-    )
-except KeyboardInterrupt:
-  serial_conn.close()
+  except KeyboardInterrupt:
+    serial_conn.close()
+
+if __name__ == '__main__':
+  main()
